f5de01649322c1db565ef94e341224357083e93d
Tobinator647 <hain.tobias@outlook.at>
2025-08-06 15:17:41 +0200

    fix: cleanup backend

--- backend-api/src/index.ts
+++ backend-api/src/index.ts
@@ -11,17 +11,6 @@ export default {
     const { pathname } = url;
     const method = request.method;
 
-    if (method === "OPTIONS") {
-      return new Response(null, {
-        status: 204,
-        headers: {
-          "Access-Control-Allow-Origin": "*", // or specify your frontend origin
-          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        },
-      });
-    }
-
     if (pathname === "/api/spotify" && method === "GET") {
       const ids: string =
         "1t20wYnTiAT0Bs7H1hv9Wt,6f0SUJsj1yDGVOIChVQCwc,77SW9BnxLY8rJ0RciFqkHh,25jJ6vyXwTRa0e6XCcdR6U,3dz0NnIZhtKKeXZxLOxCam";
