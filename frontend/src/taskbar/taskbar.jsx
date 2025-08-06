91b4ede2111872ee6a5d695a1c92111254d70c7f
Tobinator647 <hain.tobias@outlook.at>
2025-08-06 15:19:52 +0200

    style: OS clock

--- frontend/src/taskbar/taskbar.jsx
+++ frontend/src/taskbar/taskbar.jsx
@@ -31,7 +31,17 @@ export default function Taskbar() {
         </div>
         <div className="settings">
           <div className="site">Site A</div>
-          <div className="time">{time.toLocaleString()}</div>
+          <div className="time">
+            {time.toLocaleString("de-DE", {
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+              hour12: false,
+            })}
+          </div>
         </div>
       </div>
     </>
